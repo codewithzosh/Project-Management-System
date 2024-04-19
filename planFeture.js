6f2fcd45762b3f5a55807dd11498ace1c43c2a4d
Code With Zosh <133359818+codewithzosh@users.noreply.github.com>
2024-04-19 18:58:27 +0530

    Create planFeture.js

---
+++ planFeture.js
@@ -0,0 +1,31 @@
+const paidPlan = [
+  "Add unlimited project",
+  "Access to live chat",
+  "Add unlimited team member",
+  "Advanced Reporting",
+  "Priority Support",
+  "Customization Options",
+  "Integration Support",
+  "Advanced Security",
+  "Training and Resources",
+  "Access Control",
+  "Custom Workflows",
+];
+
+const annualPlan = [
+  "Add unlimited project",
+  "Access to live chat",
+  "Add unlimited team member",
+  "Advanced Reporting",
+  "Priority Support",
+  "Everything which montly plan has",
+];
+
+const freePlan = [
+  "Add only 3 projects",
+  "Basic Task Management",
+  "Project Collaboration",
+  "Basic Reporting",
+  "Email Notifications",
+  "Basic Access Control",
+];
